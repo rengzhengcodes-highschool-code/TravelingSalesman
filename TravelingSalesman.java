458e4e470b95944f5ba550e6efe70bcd1f2c25ef
Sarah Leong <sleong20@stuy.edu>
2021-01-19 18:20:11 -0500

    added to importFromFile: calculates the number of cities and creates an arraylist of Strings whose indexes are their numerical representations for the 2d array

--- TravelingSalesman.java
+++ TravelingSalesman.java
@@ -1,3 +1,6 @@
+import java.util.*;
+import java.io.*;
+
 public class TravelingSalesman {
 	public static void main (String[] args) {
 		System.out.println((int) Double.POSITIVE_INFINITY);
@@ -5,9 +8,35 @@ public class TravelingSalesman {
 
 	public static int[][] importFromFile(String file) {
 		int cities = 0;
+		ArrayList<String> citiesList = new ArrayList<String>();
 		//parse each line to cities to cities to value
+		try {
+			File f = new File(file);
+			Scanner in = new Scanner(f);
+			while (in.hasNextLine()) {
+
+				Scanner line = new Scanner(in.nextLine());
+
+				for (int i = 0; line.hasNext(); i++) {
+
+					String word = line.next();
+					// creates a list of cities with corresponding indexes and counts number of cities
+					if ((i==0||i==2) && !citiesList.contains(word)) {
+						citiesList.add(word);
+						cities++;
+					}
+				}
+				line.close();
+			}
+			in.close();
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+			System.out.println(file + "not found");
+		}
+
 		int[][] distances = new int[cities][cities];
 		//assign distances here1
+
 		return distances;
 	}
 
