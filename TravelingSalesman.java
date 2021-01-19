835faeaab322dbcba5c4e543d3fa54fed7c4e71e
rengzhengcodes <rzhengcontacts@gmail.com>
2021-01-19 13:21:23 -0500

    Added scaffold for methods discussed during class.

--- TravelingSalesman.java
+++ TravelingSalesman.java
@@ -1,5 +1,30 @@
 public class TravelingSalesman {
 	public static void main (String[] args) {
+		System.out.println((int) Double.POSITIVE_INFINITY);
+	}
+
+	public static int[][] importFromFile(String file) {
+		int cities = 0;
+		//parse each line to cities to cities to value
+		int[][] distances = new int[cities][cities];
+		//assign distances here1
+		return distances;
+	}
+
+	public static int[][] pathGeneration (int cities) {
+		//generates how many possible paths there are
+		int permutations = 1;
+		for (int counter = cities; counter >= 1; counter--) cities *= permutations;
+		//generates arrays
+		int[][] paths = new int[permutations][cities];
+
+		return paths;
+	}
+
+	public static int leastDistancePerPath (int[][] paths) {
+		int distance = (int)Double.POSITIVE_INFINITY;
 		
+
+		return distance;
 	}
 }
