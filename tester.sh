a5a305c484b7ca930661ea8def10fc80710d8828
rengzhengcodes <rzhengcontacts@gmail.com>
2021-01-21 00:40:37 -0500

    Fixed print order.

--- tester.sh
+++ tester.sh
@@ -1,4 +1,7 @@
 javac TotalTester.java
-time echo cat "Expected: 207 | Got: " "$(java TotalTester Cities0.txt)"
-time echo cat "Expected: 113 | Got: " "$(java TotalTester Cities1.txt)"
-time echo cat "Expected: 605 | Got: " "$(java TotalTester Cities2.txt)"
+echo "Expected: 207"
+time java TotalTester Cities0.txt
+echo "Expected: 113"
+time java TotalTester Cities1.txt
+echo "Expected: 605"
+time java TotalTester Cities2.txt
