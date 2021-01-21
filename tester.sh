163a3eb37cf8e49ebdc6c2872e6e6b21aa642ab0
rengzhengcodes <rzhengcontacts@gmail.com>
2021-01-21 00:17:30 -0500

    Added tester bash file.

---
+++ tester.sh
@@ -0,0 +1,9 @@
+javac TotalTester.java
+time java TotalTester Cites0.txt
+echo "Expected: 207"
+
+time java TotalTester Cities1.txt
+echo "Expected: 605"
+
+time java TotalTester Cities2.txt
+echo "Expected: 113"
